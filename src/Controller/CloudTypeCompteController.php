e865dd3ca2f6a3e9a92bbcebfcbe7f958f3b87eb
Desy2409 <ktchipotou@gmail.com>
2020-01-11 19:52:09 +0000

    no message

--- src/Controller/CloudTypeCompteController.php
+++ src/Controller/CloudTypeCompteController.php
@@ -5,52 +5,41 @@ namespace App\Controller;
 use App\Entity\CloudTypeCompte;
 use App\Form\CloudTypeCompteFormType;
 
-use Doctrine\Common\Persistence\ObjectManager;
-use Twig\Environment;
 use Symfony\Component\HttpFoundation\Request;
-//use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CloudTypeCompteController extends AbstractController
-{
+ {
     /**
-     * @Route("/cloud/type/compte", name="cloud_type_compte_new_old")
-     * Permet de créer un nouveau CloudTypeCompte
-     * @param Request $request
-     * @return Response
-     */
-    public function ajouter_old(Request $request)
-    {
-        $cloudTypeCompte = new CloudTypeCompte();
-        $form = $this->createForm(CloudTypeCompteFormType::class, $cloudTypeCompte);
-        $form->handleRequest($request);
+    * @Route( '/cloud/type/compte', name = 'cloud_type_compte_new_old' )
+    * Permet de créer un nouveau CloudTypeCompte
+    * @param Request $request
+    * @return Response
+    */
 
-        dump($request);
-        if ($form->submi) {
-            # code...
-        }
-        /*if ($form->isSubmitted()){
-        //if ($form->isSubmitted() && $form->isValid()) {
-           // $em = $this->getDoctrine()->getManager();
-            $em->persist($cloudTypeCompte);
-            $em->flush();
-            //return new Response('Type compte ajouté...')
-        }*/
+    public function ajouter( Request $request )
+ {
+        $cloudTypeCompte = new CloudTypeCompte();
+        $form = $this->createForm( CloudTypeCompteFormType::class, $cloudTypeCompte );
+        $form->handleRequest( $request );
+
+        dump( $request );
         $formView = $form->createView();
-        return $this->render('cloud_type_compte/index.html.twig', [
+        return $this->render( 'cloud_type_compte/index.html.twig', [
             'form' => $formView
-        ]);
+        ] );
     }
 
     /**
-     * @Route("/cloud/type/compte", name="cloud_type_compte")
-     */
+    * @Route( '/cloud/type/compte', name = 'cloud_type_compte' )
+    */
+
     public function index()
-    {
-        return $this->render('cloud_type_compte/index.html.twig', [
+ {
+        return $this->render( 'cloud_type_compte/index.html.twig', [
             'controller_name' => 'CloudTypeCompteController',
-        ]);
+        ] );
     }
 }
