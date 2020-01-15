bfe03962afb589e3ec602371087af7bc158abcd8
EmmanuelAmou <emmanelamou@gmail.com>
2020-01-15 10:27:36 -0500

    no

--- src/Controller/CloudTypeServiceController.php
+++ src/Controller/CloudTypeServiceController.php
@@ -55,55 +55,32 @@ class CloudTypeServiceController extends AbstractController
      * 
      * @Route
      */
+
+
     public function editer(Request $request, $id)
     {
+        $manager = $this->getDoctrine()->getManager();
+        $cloudTypeService = $manager->getRepository(CloudTypeService::class)->find($id);
+        $form = $this->createForm(CloudTypeServiceFormType::class, $cloudTypeService, array(
+            'action' => $this->generateUrl('cloudTypeservice_edit', array('id' => $id)),
+        ));
 
-
-        $form = $this->createForm(CloudTypeServiceFormType::class, $cloudTypeService);
-        $form->handleRequest($request);
-        dump($cloudTypeService);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $manager = $this->getDoctrine()->getManager();
-            $manager->persist($cloudTypeService);
-            $manager->flush();
-
-            $listCloudTypeService = $this->getListCloudTypeService();
-            //$this->addFlash("success", "Enregistrement effectué avec succes");
+        if ($request->getMethod() == 'POST') {
+            if ($form->handleRequest($request)->isValid()) {
+                $manager->flush();
+                $message = 'ok';
+                $response = new Response();
+                $response->headers->set('content-type', 'application/json');
+                $response->setContent(json_encode($message));
+                return $response;
+            }
         }
 
         return $this->render('cloud_type_service/index.html.twig', [
-            'form' => $form->createView(),
-            'listCloudTypeService' => $listCloudTypeService,
-
+            'idTypeService' => $id,
+            'cloudTypeServiceForm' => $form->createView()
         ]);
     }
-    /*
-
-    public function Edit(Request $request, $id)
-{
-    $manager = $this->getDoctrine()->getManager();
- 
-    $cloudTypeService = $manager->getRepository('CloudTypeService')->find($id);
- 
-    $form = $this->createForm(CloudTypeServiceFormType::class, $cloudTypeService);
- 
-    if($request->isXmlHttpRequest()){
-        if ($form->handleRequest($request)->isValid()) {
-            $manager->flush();
-            $message = 'OK';
-            $response = new Response();
-            $response->headers->set('Content-Type', 'application/json');
-            $response->setContent(json_encode($message));
-            return $response;
-        }
-    }
-    else {
-        return $this->render('FEGestionBundle:Gestion:edit.html.twig', array(
-            'idCloudTypeService' => $id,
-            'form' => $form->createView()
-        ));
-    }
-}
 
     /**
      * @Route("/cloud/type/service", name="cloud_type_service")
